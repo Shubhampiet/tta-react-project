da1c0d732b67889170c958b0504f49cee67edc80
Shubham Singh <52279073+Shubhampiet@users.noreply.github.com>
2023-09-22 10:49:35 +0530

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div className='footer-div'>
-<span className="footer-div-txt">
-    THIS IS THE FOOTER CONTENT WHICH IS ALIGNED LEFT
-</span>
-    </div>
+    return (
+        <div className='footer-div'>
+            <span className="footer-div-txt">
+                THIS IS THE FOOTER CONTENT WHICH IS ALIGNED LEFT
+            </span>
+        </div>
     )
 }
 
