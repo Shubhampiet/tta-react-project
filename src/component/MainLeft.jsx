1c0990af8e3fe2f238165112e312bef9110f637f
Shubham Singh <52279073+Shubhampiet@users.noreply.github.com>
2023-09-22 10:52:23 +0530

    Update MainLeft.jsx

--- src/component/MainLeft.jsx
+++ src/component/MainLeft.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const MainLeft = () => {
-  return (
-    <div>
+    return (
+        <div className='mainLeft'>
             <span className="main-left-txt">
- THIS IS SOME CONTENT ALIGNED IN THE CENTER VERTICALLY AND HORIZONTALLY
-    </span>
-    </div>
-  )
+                THIS IS SOME CONTENT ALIGNED IN THE CENTER VERTICALLY AND HORIZONTALLY
+            </span>
+        </div>
+    )
 }
 
 export default MainLeft
